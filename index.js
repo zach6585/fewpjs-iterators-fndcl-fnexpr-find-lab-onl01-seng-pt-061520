dbe7ff395ec27112e718bef166d504ff0336f1e6
zach6585 <zmines@wesleyan.edu>
2021-01-11 20:02:42 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(objArray) {
+  let win = objArray.find(function(s) { return s["result"] === "W"});
+  return win ? win["year"] : undefined
+}
